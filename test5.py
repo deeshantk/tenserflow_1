fdb8ea04ed7ea476358c1600f78ac5c2787aaafe
deeshantk <44977226+deeshantk@users.noreply.github.com>
2019-07-27 00:36:51 +0530

    Update test5.py

--- test5.py
+++ test5.py
@@ -7,8 +7,8 @@ model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])]) # De
 model.compile(optimizer='sgd', loss='mean_squared_error')
 
 # Providing data
-xs = np.array([-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16], dtype=int)
-ys = np.array([-2, 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32], dtype=int)
+xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0], dtype=float)
+ys = np.array([-2.0, 0.0, 2.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0, 16.0, 18.0, 20.0, 22.0, 24.0, 26.0, 28.0, 30.0, 32.0], dtype=float)
 
 # Training neural net
 model.fit(xs, ys, epochs=4000)
