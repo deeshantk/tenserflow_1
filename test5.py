970a2f4b3044256f64e46c524da224671a005c1a
deeshantk <44977226+deeshantk@users.noreply.github.com>
2019-07-30 22:50:23 +0530

    Update test5.py

--- test5.py
+++ test5.py
@@ -12,4 +12,4 @@ ys = np.array([-2.0, 0.0, 2.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0, 16.0, 18.0, 20.0
 
 # Training neural net
 model.fit(xs, ys, epochs=4000)
-print(model.predict([50.0]))
+print(model.predict([50.0]))  # Will predict value for ys = 50.0
